919b9c2ba1d9ff2182f2a037756ab17f3180e408
Maleeha Babar <mbabar@tulane.edu>
2025-04-03 12:04:22 -0500

    changes

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pandas as pd
 st.title("My Streamlit App")
 
 if st.button("Load Data"):
-    # Load data from CSV file
-    df = pd.read_csv("data.csv")
-    st.write(df)
+    if st.file_uploader("Upload a CSV file", type=["csv"]) is not None:
+        # Load data from uploaded CSV file
+        df = pd.read_csv(st.file_uploader("Upload a CSV file", type=["csv"]))
+        st.write(df)
+    else:
+        st.warning("Please upload a CSV file to load data.")
