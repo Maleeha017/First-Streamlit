351e9d1a46a62f9e9d20452b485922d9a90bc538
Maleeha Babar <mbabar@tulane.edu>
2025-04-03 12:02:11 -0500

    Adding requirements

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title("My Streamlit App")
-st.write("Hello, world!")
-st.button("Click me!")
+
+if st.button("Load Data"):
+    # Load data from CSV file
+    df = pd.read_csv("data.csv")
+    st.write(df)
